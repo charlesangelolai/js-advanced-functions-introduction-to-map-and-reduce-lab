f8d0ce94ca678becccc9051e74922a433efb618d
Charles Lai <charlesangeloplai@gmail.com>
2021-03-04 21:44:48 -0500

    commit tree f0b5614c316e4a9618c636d88cccd13b2c463a8e

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    sourceArray[i] *= -1;
+  }
+
+  return sourceArray;
+}
+
+function mapToNoChange(sourceArray) {
+  return sourceArray;
+}
+
+function mapToDouble(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    sourceArray[i] *= 2;
+  }
+
+  return sourceArray;
+}
+
+function mapToSquare(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    sourceArray[i] = Math.pow(sourceArray[i], 2);
+  }
+
+  return sourceArray;
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  let total = startingPoint;
+
+  for (let i = 0; i < sourceArray.length; i++) {
+    total += sourceArray[i];
+  }
+
+  return total;
+}
+
+function reduceToAllTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (!sourceArray[i]) return false;
+  }
+
+  return true;
+}
+
+function reduceToAnyTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i]) return true;
+  }
+
+  return false;
+}
